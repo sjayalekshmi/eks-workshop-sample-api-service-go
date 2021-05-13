dfcb1ce622a9569dbe16fb4fe09a42f58f78edc1
Sasikala Jayalekshmi <sjayalekshmi@ebsco.com>
2021-05-13 16:50:29 -0400

    eksworkshop

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Codepipeline Setup- Sas workshop"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
